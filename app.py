748da31f43694665157f266a5029d6ce334cad07
ondcapek <ocapek@gmail.com>
2024-09-25 10:43:08 +0200

    first wirking page with form

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify, request
 from dominate import document
 from dominate.tags import *
-from app.forms import FormAccount
+from app.forms import FormAccount, NameForm
 from flask import render_template, flash, redirect
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
+
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
+app.secret_key = 'tO$&!|0wkamvVia0?n$NqIRVWOG'
+bootstrap = Bootstrap5(app)
+csrf = CSRFProtect(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -32,6 +41,19 @@ def sqr():
     number = request.json['number']
     res = [{'result': number * number}]
     return jsonify(res), 200
+
+@app.route('/actor', methods=['GET', 'POST'])
+def actor():
+    names = ['Pepa', "Fanda, 'Tonda"]
+    form = NameForm()
+    message = ""
+    if form.validate_on_submit():
+        name = form.name.data
+        if name.lower() in names:
+            message = "That actor is in our database."
+        else:
+            message = "That actor is not in our database."
+    return render_template('index.html', names=names, form=form, message=message)
 
 
 if __name__ == '__main__':
--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, DataRequired, Length
 from wtforms import StringField, EmailField, validators, SubmitField
 from flask_wtf import FlaskForm
 
@@ -6,3 +6,7 @@ class FormAccount(FlaskForm):
     username = StringField('Tvoje jmeno', validators=[validators.InputRequired()])
     email = EmailField('Email', validators=[InputRequired(), validators.Length(4, 128), validators.Email()])
     submit = SubmitField('Pridej')
+
+class NameForm(FlaskForm):
+    name = StringField('Which actor is your favorite?', validators=[DataRequired(), Length(2, 10)])
+    submit = SubmitField('Submit')
